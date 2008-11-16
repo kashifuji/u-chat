8a9039a69b3fd81f0b3e6075710388ca8aec6ba6
Yoshitaka Kashifuji <katashiyo515@yahoo.co.jp>
2008-11-16 16:25:09 +0900

    add function

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -176,7 +176,22 @@ class ChatController < ApplicationController
             :user_data => session[:privID], :roomID => session[:roomID] ) 
          if @chat.save
             content2 = render_component_as_string :action => 'show', :id => @chat.id
-            content = render_component_as_string :action => 'test', :id => @chat.id
+            @pos_x = params[:hf_x].to_i
+            @pos_y = params[:hf_y].to_i
+            if params[:pos] == 'checked'
+               @pos_x = params[:hf_x].to_i
+               @pos_y = params[:hf_y].to_i - session[:n_size].to_i / 2
+               if @pos_x > 2000
+                  @pos_x = 2000
+               end
+               if @pos_y > 2000
+                  @pos_y = 2000
+               end
+            else
+               @pos_x = rand(400)
+               @pos_y = rand(400)
+            end
+            content = render_component_as_string :action => 'test', :id => @chat.id, :params => { "pos_x" => @pos_x, "pos_y" => @pos_y }
             javascript = render_to_string :update do |page|
                page.insert_html :top, 'chat-list', content
                if params[:Effects] == 'Highlight'
